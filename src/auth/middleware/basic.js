b996961367847fa687126fa3bfc183b41bb3faad
Aseelsamer <assoul_08@hotmail.com>
2021-01-11 20:29:49 +0200

    lab11

---
+++ src/auth/middleware/basic.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const base64 = require('base-64');
+const users = require('../models/user-model');
+
+
+module.exports = (req, res, next)=> {
+    if (!req.headers.authorization)  {
+        next('invaid login');
+        return;
+    }
+    let authHeader = req.headers.authorization.split(" ");
+    if (authHeader[0] != "Basic") {
+        next('invaid login');
+        return; 
+    }
+    let basic = authHeader.pop();
+    let [username, password] = base64.decode(basic).split(":");//i have them now i wanto to validate if it is correct or not
+    users.authenticateBasic(username, password).then(verified=>{
+        req.users=verified;
+        users.generateToken(verified).then(generatedToken=> {
+            req.token = generatedToken;
+            next();
+        }).catch(err=> next('error in Token!'))
+    
+    }).catch(err=> next('Invalid Login!'))
+
+}
---
+++ src/auth/router.js
@@ -0,0 +1,30 @@
+'use strict';
+const express = require('express');
+const users = require('./models/user-model')
+const router = express.Router();
+const authenticateBasic = require('../auth/middleware/basic')
+
+//routes
+router.post('/signup',(req,res)=>{
+    users.save(req.body.username)
+    .then(user=>{
+        users.generateToken(user).then(result=> {
+            // console.log(result);
+            res.status(200).send(result);
+        });
+    }).catch(e=> res.status(403).send("creating user error!"));
+})
+
+router.post('/signin',authenticateBasic,(req,res)=>{
+    res.set('authorization',req.token);
+    res.cookie('token',res.token);
+res.status(200).send({users:req.users,token:req.token});
+})
+
+router.get('/users',authenticateBasic,(req,res)=>{
+    users.get().then((result)=>{
+        res.json({result})
+    })
+})
+
+module.exports =router;
